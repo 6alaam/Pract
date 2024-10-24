ed5075eab63bfd5a456d26ecdcbf1f6e0ca9ce1d
Balaam <ballaam@yandex.ru>
2024-10-24 15:12:39 +0300

    создание структуры сделано 1 задание сделана первая тема добавил задания

---
+++ src/ru/yandex/practicum/codingpractice/work/MKS/ISS.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.codingpractice.work.MKS;
+
+public class ISS extends Zarya {
+    public ISS(double initialOxygenLevel, double initialEnergyLevel, double initialFuel) {
+
+        // требуется увеличить уровень кислорода и энергетических резервов
+        super(initialOxygenLevel, initialEnergyLevel);
+
+        // доступ к изменению полей запрещён
+        fuel = initialFuel;
+        vibration = 0;
+        energyConsumption = 20;
+    }
+
+    // доступ к изменению метода запрещён
+    public void moduleDocking(Zvezda module) {
+        connectEnergyCableTo(module);
+        connectOxygenCableTo(module);
+        stabilize();
+    }
+
+    // получен доступ к переопределению метода стабилизации
+    @Override
+    public void stabilize(){  // получен доступ к переопределению метода стабилизации
+        for (int i = 0; i < 3;i++){
+            stabilizeBySmallEngines();
+        }
+    }
+    // доступ к изменению метода запрещён
+    private void connectOxygenCableTo(Zvezda module) {
+        double oxygenLosses = (oxygenLevel + module.oxygenLevel) / 2 + 9;
+        oxygenLevel -= oxygenLosses;
+        vibration += 2;
+    }
+
+    // доступ к изменению метода запрещён
+    private void connectEnergyCableTo(Zvezda module) {
+        energyReserve += module.getEnergyReserve();
+        energyConsumption += module.getEnergyConsumption() * 1.3;
+        vibration += 2;
+    }
+}
---
+++ src/ru/yandex/practicum/codingpractice/work/SOUPER3000/Bouillon.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.codingpractice.work.SOUPER3000;
+
+public class Bouillon extends SoupBase {
+    double meat;
+
+    // реализуйте необходимые конструкторы
+    public Bouillon(SoupBase soupBase, double meat) {
+        super(soupBase.water, soupBase.salt);
+        this.meat = meat;
+    }
+
+   public  Bouillon(double water, double salt,double meat){
+        super(water, salt);
+       this.meat = meat;
+   }
+
+//    public Bouillon(double water, double salt, double meat) {
+//        super(water, salt);
+//    }
+
+    @Override
+    public void printIngredients() {
+        System.out.println("Ингредиенты бульона:");
+        System.out.println("Вода: " + water + " л.");
+        System.out.println("Соль: " + salt + " г.");
+        System.out.println("Мясо: " + meat + " г.");
+    }
+}
